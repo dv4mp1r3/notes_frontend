68eda27d457c48ccea9b3c3180c7ea3f65025dd8
dv4mp1r3 <somebody@somewhere>
2024-05-31 00:26:36 +0300

    implemented encryption key gui behaviour

---
+++ src/components/EncryptionKeyEditor.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="editor">
+        <textarea v-model="data"></textarea>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, toNative, Prop } from 'vue-facing-decorator'
+
+@Component
+class EncryptionKeyEditor extends Vue {
+
+    @Prop
+    minHeight: number = 640;
+
+    get data() : string {
+        return this.$store.getters.getEncryptionKey;
+    }
+
+    set data(s: string) {
+        this.$store.dispatch('setEncryptionKey', s);
+    }
+
+    mounted() {
+        document.body.classList.add('align-flex-start');
+    }
+    
+}
+
+export default toNative(EncryptionKeyEditor)
+</script>
+<style scoped>
+.editor {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+</style>
